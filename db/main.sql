4976e525b5ccac94aef64c77b6232337f1021f21
michalmity <gemcz295@gmail.com>
2024-06-17 19:16:30 +0200

    Vytvořena database

---
+++ db/main.sql
@@ -0,0 +1,41 @@
+#drop database if exists taskmanager;
+create database if not exists taskmanager;
+
+use taskmanager;
+
+create table users (
+    user_id int primary key auto_increment,
+    username varchar(255) not null,
+    password varchar(255) not null,
+    email varchar(255) not null,
+    role_id int not null
+);
+
+create table roles (
+    role_id int primary key auto_increment,
+    name varchar(255) not null
+);
+
+create table tasks (
+    task_id int primary key auto_increment,
+    title varchar(255) not null,
+    description text not null,
+    created_at datetime not null,
+    updated_at datetime not null,
+    end_at datetime not null,
+    status_id int not null,
+    user_id int not null
+);
+
+create table statuses (
+    status_id int primary key auto_increment,
+    name varchar(255) not null
+);
+
+alter table users add constraint fk_users_roles foreign key (role_id) references roles(role_id);
+alter table tasks add constraint fk_tasks_statuses foreign key (status_id) references statuses(status_id);
+alter table tasks add constraint fk_tasks_users foreign key (user_id) references users(user_id);
+
+insert into roles (name) values ('admin');
+insert into roles (name) values ('user');
+
---
+++ db/procedures.sql
@@ -0,0 +1,30 @@
+use taskmanager;
+
+DELIMITER $$
+
+create procedure add_user(
+    in p_username varchar(50),
+    in p_email varchar(50),
+    in p_password varchar(256)
+)begin
+    insert into users(username, email, password, role_id) values(p_username, p_email, p_password, 2);
+
+end;
+
+
+DELIMITER ;
+
+DELIMITER $$
+
+create procedure add_task(
+    in p_title varchar(50),
+    in p_description text,
+    in p_created_at datetime,
+    in p_updated_at datetime,
+    in p_end_at datetime,
+    in p_status_id int,
+    in p_user_id int
+)begin
+    insert into tasks(title, description, created_at, updated_at, end_at, status_id, user_id) values(p_title, p_description, p_created_at, p_updated_at, p_end_at, p_status_id, p_user_id);
+
+end;
