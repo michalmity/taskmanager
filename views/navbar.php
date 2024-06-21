940db3f16dd4afb93183cd3aab536bd647e5ba3f
michalmity <gemcz295@gmail.com>
2024-06-21 23:50:48 +0200

    edited nav

--- views/navbar.php
+++ views/navbar.php
@@ -7,9 +7,10 @@
             <div class="navbar-profile">
                     <img src="https://wallpapers.com/images/featured-full/cool-profile-picture-87h46gcobjl5e4xu.jpg">
                 <p><?php echo $_SESSION["username"]; ?></p>
-                <a href="../controllers/user_controller.php?action=logout">
-                    <i class="bi bi-box-arrow-right"></i>
-                </a>
+                <form action="../controllers/user_controller.php" method="post">
+                    <input type="hidden" name="action" value="logout">
+                    <button type="submit"><i class=" bi bi-box-arrow-right"></i></button>
+                </form>
             </div>
         <?php endif; ?>
     </div>
