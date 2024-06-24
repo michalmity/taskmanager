5ccc71cf390696e89cf480093f0c76ca1eb1047d
michalmity <gemcz295@gmail.com>
2024-06-24 17:26:14 +0200

    Updated task actions

--- index.php
+++ index.php
@@ -13,28 +13,36 @@
 <body>
 <?php require_once __DIR__ . '/views/navbar.php' ?>
 <?php if (isset($_SESSION["user_id"])): ?>
-<div class="list-container">
-    <div class="list to-do-list">
-        <h3>Todo</h3>
-        <div class="item-container">
-            <?php require_once "controllers/task_controller.php";
-            $controller = new controllers\task_controller();
-            $tasks = $controller->get_tasks(1);
-            foreach ($tasks as $task) {
-                echo "<div class='list-item'>
+    <div class="list-container">
+        <div class="list to-do-list">
+            <h3>Todo</h3>
+            <div class="item-container">
+                <?php require_once "controllers/task_controller.php";
+                $controller = new controllers\task_controller();
+                $tasks = $controller->get_tasks(1);
+                foreach ($tasks as $task) {
+                    echo "<div class='list-item'>
                     <div class='list-item-content'>
                         <p class='task-description'>{$task['description']}</p>
                         <i class='task-due'>{$task['due_to']}</i>
                     </div>
                     <div class='list-item-actions'>
-                        <i class='bi bi-pencil'></i>
-                        <i class='bi bi-trash'></i>
+                        <form action='controllers/task_controller.php' method='post'>
+                            <input type='hidden' name='action' value='update_task'>
+                            <input type='hidden' name='task_id' value='{$task['id']}'>
+                            <button type='submit'><i class='bi bi-pencil'></i></button>
+                        </form>
+                        <form action='controllers/task_controller.php' method='post'>
+                            <input type='hidden' name='action' value='delete_task'>
+                            <input type='hidden' name='task_id' value='{$task['id']}'>
+                            <button type='submit'><i class='bi bi-trash'></i></button>
+                        </form>
                     </div>
                 </div>";
-            }
-            ?>
+                }
+                ?>
+            </div>
         </div>
-    </div>
         <div class="list working-on-list">
             <h3>Working on</h3>
             <div class="item-container">
@@ -47,8 +55,16 @@
                         <i class='task-due'>{$task['due_to']}</i>
                     </div>
                     <div class='list-item-actions'>
-                        <i class='bi bi-pencil'></i>
-                        <i class='bi bi-trash'></i>
+                        <form action='controllers/task_controller.php' method='post'>
+                            <input type='hidden' name='action' value='update_task'>
+                            <input type='hidden' name='task_id' value='{$task['id']}'>
+                            <button type='submit'><i class='bi bi-pencil'></i></button>
+                        </form>
+                        <form action='controllers/task_controller.php' method='post'>
+                            <input type='hidden' name='action' value='delete_task'>
+                            <input type='hidden' name='task_id' value='{$task['id']}'>
+                            <button type='submit'><i class='bi bi-trash'></i></button>
+                        </form>
                     </div>
                 </div>";
                 }
@@ -67,8 +83,16 @@
                         <i class='task-due'>{$task['due_to']}</i>
                     </div>
                     <div class='list-item-actions'>
-                        <i class='bi bi-pencil'></i>
-                        <i class='bi bi-trash'></i>
+                        <form action='controllers/task_controller.php' method='post'>
+                            <input type='hidden' name='action' value='update_task'>
+                            <input type='hidden' name='task_id' value='{$task['id']}'>
+                            <button type='submit'><i class='bi bi-pencil'></i></button>
+                        </form>
+                        <form action='controllers/task_controller.php' method='post'>
+                            <input type='hidden' name='action' value='delete_task'>
+                            <input type='hidden' name='task_id' value='{$task['id']}'>
+                            <button type='submit'><i class='bi bi-trash'></i></button>
+                        </form>
                     </div>
                 </div>";
                 }
@@ -98,38 +122,38 @@
             <button type="submit" class="button-submit">Add</button>
         </form>
     </div>
-    <?php else: ?>
-        <div class="container" id="container">
-            <div class="form-container" id="form-container">
-                <div class="form" id="form-login">
-                    <h2>Přihlášení</h2>
-                    <form action="../controllers/user_controller.php" method="post">
-                        <input type="hidden" name="action" value="login">
-                        <label for="email">Email:</label><br>
-                        <input type="email" id="email_login" name="email" required><br>
-                        <label for="password">Heslo:</label><br>
-                        <input type="password" id="heslo_login" name="password" required><br><br>
-                        <button type="submit" class="button-submit">Login</button>
-                    </form>
-                    <p>Nemáš účet? <a id="show-register">Zaregistrovat se</a></p>
-                </div>
-                <div class="form" id="form-register" style="display: none;">
-                    <h2>Registrace</h2>
-                    <form action="../controllers/user_controller.php" method="post">
-                        <input type="hidden" name="action" value="register">
-                        <label for="username">Uživatelské jméno:</label><br>
-                        <input type="text" id="username" name="username" required><br>
-                        <label for="email">Email:</label><br>
-                        <input type="email" id="email_register" name="email" required><br>
-                        <label for="heslo">Heslo:</label><br>
-                        <input type="password" id="heslo_register" name="password" required><br>
-                        <button class="button-submit" type="submit">Register</button>
-                    </form>
-                    <p>Máš účet? <a id="show-login">Přihlásit se</a></p>
-                </div>
+<?php else: ?>
+    <div class="container" id="container">
+        <div class="form-container" id="form-container">
+            <div class="form" id="form-login">
+                <h2>Přihlášení</h2>
+                <form action="../controllers/user_controller.php" method="post">
+                    <input type="hidden" name="action" value="login">
+                    <label for="email">Email:</label><br>
+                    <input type="email" id="email_login" name="email" required><br>
+                    <label for="password">Heslo:</label><br>
+                    <input type="password" id="heslo_login" name="password" required><br><br>
+                    <button type="submit" class="button-submit">Login</button>
+                </form>
+                <p>Nemáš účet? <a id="show-register">Zaregistrovat se</a></p>
+            </div>
+            <div class="form" id="form-register" style="display: none;">
+                <h2>Registrace</h2>
+                <form action="../controllers/user_controller.php" method="post">
+                    <input type="hidden" name="action" value="register">
+                    <label for="username">Uživatelské jméno:</label><br>
+                    <input type="text" id="username" name="username" required><br>
+                    <label for="email">Email:</label><br>
+                    <input type="email" id="email_register" name="email" required><br>
+                    <label for="heslo">Heslo:</label><br>
+                    <input type="password" id="heslo_register" name="password" required><br>
+                    <button class="button-submit" type="submit">Register</button>
+                </form>
+                <p>Máš účet? <a id="show-login">Přihlásit se</a></p>
             </div>
         </div>
-    <?php endif; ?>
+    </div>
+<?php endif; ?>
 </body>
 </html>
 
