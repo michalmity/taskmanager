92387e319f801d26aa2c844ea5310f7dfe3c28be
michalmity <gemcz295@gmail.com>
2024-06-25 21:24:01 +0200

    modal js

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+
+
+function hideModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+function showModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+
+document.getElementById('modal-close').addEventListener('click', hideModal);
+
+// Zavřít modal při kliknutí mimo modalní okno
+window.onclick = function(event) {
+    if (event.target == document.getElementById('modal')) {
+        hideModal();
+    }
+}
