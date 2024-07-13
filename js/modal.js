27fa244a7841733e147e2ab5376fad9909592f90
michalmity <gemcz295@gmail.com>
2024-07-13 23:41:30 +0200

    Working both on mobile and desktop

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,19 @@ var addTaskModal = document.getElementById('add-task-modal');
 var editTaskModal = document.getElementById('edit-task-modal');
 
 // Získání tlačítek pro otevření modálních oken
-var addTaskBtn = document.getElementById('nav-add');
+var addTaskBtn1 = document.getElementsByClassName('add-task-btn')[0];
+var addTaskBtn2 = document.getElementsByClassName('add-task-btn')[1];
 var editTaskBtns = document.getElementsByClassName('edit-task-btn');
 
 // Získání prvků pro zavření modálních oken
 var closeButtons = document.getElementsByClassName('close');
 
 // Otevření modálního okna pro přidání úkolu
-addTaskBtn.onclick = function () {
+addTaskBtn1.onclick = function () {
+    addTaskModal.style.display = 'block';
+}
+
+addTaskBtn2.onclick = function () {
     addTaskModal.style.display = 'block';
 }
 
